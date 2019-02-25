bf9a47b340cc2df5c3d9179c09a2a2e0a3bb5251
Oluwaseye <seyz4all@gmail.com>
2019-02-25 06:18:17 -0500

    Wrapped up with 1440 viewport

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,8 +17,10 @@ $(window).scroll(function () {
     $('.main').css('margin-top', '0px');
   }
   if ($(window).scrollTop() + $(window).height() - 100 == getDocHeight() - 100 ) {
+    $('.side_ad .bottom').addClass('scrolled-bottom');
     $('.footer').addClass('footer-fixed');
   }else{
+    $('.side_ad .bottom').removeClass('scrolled-bottom');
     $('.footer').removeClass('footer-fixed');
   }
 });
